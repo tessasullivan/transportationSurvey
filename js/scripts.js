9f3f960f32b1d32332ecbfbe230e027d9f042727
Tessa Sullivan and Zema Kebede <tessa@>
2019-04-02 09:53:42 -0700

    Add checkboxes for modes of transportation to activities outside of work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
-    console.log("Submitted!");
+
     $("#work-responses").show();
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
     });
     $('#transportation_survey').hide();
   });
